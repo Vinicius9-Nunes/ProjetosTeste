9d100de99c8eb43496c53e1ec7a6cdce95ce1ad9
Vinicius9-Nunes <viniciusonunes0@gmail.com>
2020-12-16 16:33:45 -0300

    Add atributo CPF

--- CadastroClientes.Core.Models/Clientes/Clientes.cs
+++ CadastroClientes.Core.Models/Clientes/Clientes.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CadastroClientes.Core.Models.Clientes
+﻿namespace CadastroClientes.Core.Models.Clientes
 {
     public class Clientes
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string Cpf { get; set; }
 
         public Clientes(string nome, int idade)
         {
